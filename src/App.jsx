eb4e565a7dbdc33b8f86d24cbe5d41a3ededa675
Giovanny Vargas <yovaz05@gmail.com>
2023-11-01 18:46:38 -0500

    clase 01/11/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<p>HomePage</p>} />
+          <Route path="/dashboard" element={<p>Dashboard</p>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
